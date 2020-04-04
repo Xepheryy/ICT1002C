a0116aba587dd5865bf32a34f9b21340f0bd0411
Shaun <tanweishaun@gmail.com>
2020-04-04 13:01:20 +0800

    Comments, code cleanup

--- hashtable.c
+++ hashtable.c
@@ -5,10 +5,10 @@
 #include <ctype.h>
 #include "chat1002.h"
 
-
 #define TABLE_SIZE 1000
 /*
-Function to create the hash table with the specified TABLE_SIZE
+Function to create the hash table with the specified TABLE_SIZE.
+Returns a pointer to the hash_table created.
 */
 hash_table *create_hash_table(){
 	//create pointer to table for reference
@@ -37,6 +37,10 @@ unsigned long hash(const char * str){
     return magic_number % TABLE_SIZE;
 }
 
+/*
+This function performs insertion into a specified hashtable.
+It returns 0 on success.
+*/
 int insert_into_hash_table(hash_table *hashTable, chat_entry *chatEntry) {
 	//generate hash for array position
 	unsigned long position = hash(chatEntry->key);
@@ -49,40 +53,51 @@ int insert_into_hash_table(hash_table *hashTable, chat_entry *chatEntry) {
 	}
 }
 
-void ht_dump(hash_table *hashtable) {
-    for (int i = 0; i < TABLE_SIZE; ++i) {
-        chat_entry *entry = hashtable->chat_entries[i];
+/*
+Useful debugging function to print everything stored in the specified hashtable.
+*/
+// void ht_dump(hash_table *hashtable) {
+//     for (int i = 0; i < TABLE_SIZE; ++i) {
+//         chat_entry *entry = hashtable->chat_entries[i];
 
-        if (entry == NULL) {
-            continue;
-        }
+//         if (entry == NULL) {
+//             continue;
+//         }
 
 
-        for(;;) {
-            printf("%s\t", entry->key);
+//         for(;;) {
+//             printf("%s\t", entry->key);
 			
-            printf("%s\t", entry->intent);
+//             printf("%s\t", entry->intent);
 			
-            printf("%s\t", entry->entity);
+//             printf("%s\t", entry->entity);
 			
-            printf("%s\n", entry->response);
+//             printf("%s\n", entry->response);
 
-            if (entry->next == NULL) {
-                break;
-            }
+//             if (entry->next == NULL) {
+//                 break;
+//             }
 
-            entry = entry->next;
-        }
+//             entry = entry->next;
+//         }
 
-        printf("\n");
-    }
-}
+//         printf("\n");
+//     }
+// }
 
-//Function to clear hashtable
+/*
+helper function to clear hashtable.
+This is a void function.
+*/
 void clearHashTable(hash_table *clearHashTable){
   free(clearHashTable);
 }
 
+/*
+Takes in hash table, intent and entity.
+Performs a dynamic lookup on the hash_table.
+Returns a pointer to the retrieved chat_entry.
+*/
 chat_entry *retrieve_chat_entry(hash_table *hashTable, const char *intent, const char *entity)
 {
 	char *key = (char *) malloc (strlen(intent) + strlen(entity) + 1);
@@ -93,6 +108,11 @@ chat_entry *retrieve_chat_entry(hash_table *hashTable, const char *intent, const
 	return chatEntry;
 }
 
+/*
+Helper function that creates a chat entry based on the intent, entity and response.
+Returns a pointer to the created chat_entry.
+To be used in conjunction with insert_into_hash_table.
+*/
 chat_entry *create_chat_entry(const char* intent,const char* entity, const char* response) {
 	chat_entry *chatEntry;
 	chatEntry = malloc(sizeof(chat_entry));
@@ -116,16 +136,3 @@ chat_entry *create_chat_entry(const char* intent,const char* entity, const char*
 	return chatEntry;
 
 }
-
-// int main() {
-// 	hash_table *hashTable = create_hash_table();
-// 	chat_entry chatEntry1 = create_chat_entry("What", "SIT", "an institution");
-	
-// 	//insert into hashtable
-// 	insert_into_hash_table(hashTable, &chatEntry1);
-// 	ht_dump(hashTable);
-// 	return 0;
-// }
-
-
-	
